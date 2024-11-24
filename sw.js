fa0257388ce8f2d46c10f8005f10579245171cfb
LuckyGirl <2668035293@qq.com>
2024-11-24 18:56:20 +0800

    Site updated: 2024-11-24 18:56:20

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732445650359";
+const VERSION = "1732445779177";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
